3d8ab5e3a548d986e31f46df9f57f0c61d927230
Diego Boschetti <dlboschetti@gmail.com>
2020-07-14 00:54:38 -0300

    first commit

---
+++ ControleAcesso.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algoritimo2
+{
+    class ControleAcesso
+    {
+        string login = "dlboschetti";
+        string senha = "123456";
+
+        public bool EfetuarLogin()
+        {
+            bool retorno = false;
+            Console.WriteLine("Digite o login:");
+            string loginDigitado = Console.ReadLine();
+
+
+
+            if (login == loginDigitado)
+            {
+                int count = 0;
+                while (count < 3)
+                {
+                    Console.WriteLine("Digite a senha:");
+                    string senhaDigitada = Console.ReadLine();
+
+                    if (senha == senhaDigitada)
+                    {
+                        retorno = true;
+                        break;
+
+                    }
+                    count++;
+                }
+            }
+
+
+            return retorno;
+        }
+
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Algoritimo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            ControleAcesso controleAcesso = new ControleAcesso();
+
+            bool logado = controleAcesso.EfetuarLogin();
+
+            if (logado)
+                Console.WriteLine("LOGIN EFETUADO COM SUCESSO!");
+
+            // Números Primos
+
+            //Console.WriteLine("Gerar números primos até o número informado, digite um número:");
+            //int limite = Convert.ToInt32( Console.ReadLine());
+
+            //int[] indices = new int[4];
+            //indices[0] = 2;
+            //indices[1] = 3;
+            //indices[2] = 5;
+            //indices[3] = 7;
+
+
+
+            //for (int l = 0; l <= limite; l++) // varrer até a limite informado
+            //{
+            //    bool primo = true;
+            //    for (int i = 0; i < 4; i++)
+            //    {
+            //        int resto = l % indices[i];
+            //        if (l != indices[i] && resto == 0)
+            //        {
+            //            primo = false;
+            //            break;
+            //        }
+
+            //    }   
+            //    if (primo)
+            //    {
+            //        Console.WriteLine(l);
+
+            //    }
+            //}
+
+
+
+            // Calculo IMC
+            //Console.WriteLine("Digite seu peso em KG:");
+            //int peso = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Digite sua altura em metros:");
+            //double altura = Convert.ToDouble( Console.ReadLine());
+
+            //double imc = peso / (altura * altura);
+            //Console.WriteLine($"imc: {imc}");
+
+            //string resultado = "";
+
+            //if (imc < 17) resultado = "Muito abaixo do peso!";
+            //if (imc >= 17 && imc  < 18.49) resultado = "Abaixo do peso!";
+            //if (imc >= 18.49 && imc < 25) resultado = "Peso normal!";
+            //if (imc >= 25 && imc < 30) resultado = "Acima do Peso";
+            //if (imc >= 30 && imc < 35) resultado = "Obesidade I";
+            //if (imc >= 35 && imc < 40) resultado = "Obesidade II (Severa)";
+            //if (imc >= 40) resultado = "Obesidade III (Morbida)";
+
+            //Console.WriteLine(resultado);
+
+        }
+    }
+}
